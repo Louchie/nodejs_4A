07245d80a65bedddbab19947471d963eccc011fb
Tibo R <thibault.raspail@gmail.com>
2024-03-25 17:41:04 +0100

    savejs avancement v2

--- save.js
+++ save.js
@@ -46,6 +46,41 @@ async function main() {
     'Salade de Fruits Frais':{ type_lunch:'dinner',taste:'salée',number_people:'4'},
   }
 
+  const recepes = {
+    'Quiche Lorraine':{ steps:' Ingrédients 1 pâte brisée 200g de lardons fumés100g de gruyère râpé 3 œufs 20cl de crème fraîche 20cl de lait Sel, poivre, muscade Instructions :Préchauffez votre four à 180°C (thermostat 6).Étalez la pâte brisée dans un moule à tarte préalablement beurré et piquez le fond à l aide d une fourchette.Dans une poêle, faites revenir les lardons à feu moyen jusqu à ce qu ils soient dorés. Égouttez-les sur du papier absorbant pour enlever l excès de gras.Répartissez les lard'},
+    'Bruschetta aux Tomates et à la Mozzarella':{ steps:'dinner'},
+    'Mini-Pizzas aux Légumes Grillés':{ steps:'dinner'},
+    'Tartelettes aux Épinards et à la Feta':{ steps:'dinner'},
+    'Poivrons Farcis au Riz et à la Viande':{ steps:'dinner'},
+    'Salade de Pommes de Terre et de Thon':{ steps:'dinner'},
+    'Toasts à l\'Avocat et aux Crevettes ':{ steps:'dinner'},
+    'Omelette aux Champignons et au Fromage':{ steps:'dinner'},
+    'Roulés de Courgettes au Fromage de Chèvre':{ steps:'dinner'},
+    'Club Sandwich au Poulet et au Bacon':{ steps:'dinner'},
+    'Gnocchis à la Sauce Tomate et Basilic':{ steps:'dinner'},
+    'Chili Con Carne avec Riz':{ steps:'dinner'},
+    'Salade de Pâtes aux Légumes Grillés':{ steps:'dinner'},
+    'Soupe de Lentilles et Saucisse':{ steps:'dinner'},
+    'Sandwich au Saumon Fumé et Fromage Frais':{ steps:'dinner'},
+    'Salade Caprese':{ steps:'dinner'},
+    'Crêpes à la Confiture de Fraises':{ steps:'dinner'},
+    'Gaufres aux Fruits et Crème Chantilly':{ steps:'dinner'},
+    'Tartelettes aux Fruits de Saison ':{ steps:'dinner'},
+    'Pancakes aux Myrtilles et Sirop d\'Érable':{ steps:'dinner'},
+    'Pudding au Pain et aux Raisins Secs':{ steps:'dinner'},
+    'Brownies aux Noix et au Chocolat ':{ steps:'dinner'},
+    'Muffins aux Pépites de Chocolat ':{ steps:'dinner'},
+    'Tarte aux Pommes Maison':{ steps:'dinner'},
+    'Crumble aux Fruits Rouges ':{ steps:'dinner'},
+    'Mousse au Chocolat':{ steps:'dinner'},
+    'Tiramisu':{ steps:'dinner'},
+    'Cheesecake aux Fruits Rouges ':{ steps:'dinner'},
+    'Tarte Tatin aux Pommes':{ steps:'dinner'},
+    'Panna Cotta à la Vanille':{ steps:'dinner'},
+    'Coulant au Chocolat':{ steps:'dinner'},
+    'Salade de Fruits Frais':{ steps:'dinner'},
+  }
+
   for (plat_name in plats) {
     await db.createplat(plat_name, plats[plat_name].qty, plats[plat_name].price);
   }
